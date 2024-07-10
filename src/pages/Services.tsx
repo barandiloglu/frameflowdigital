26877a89a789c54b098d622bdaa1046a4a4d8aca
barandiloglu <barandiloglu@gmail.com>
2024-07-10 10:34:11 -0400

    Services

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,8 +1,7 @@
 import transition from "../transition";
-import { motion, useAnimation, useMotionValue, useTransform, useSpring} from 'framer-motion';
-import { useEffect, useRef } from 'react';
-import { Link } from 'react-router-dom';
-import { FiArrowRight } from "react-icons/fi";
+import { motion, useAnimation } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { GoArrowUpRight } from "react-icons/go";
 
 const services = [
     { heading: "Social Media Management", subheading: "See our projects", imgSrc: "./src/assets/posts/1.png", href: "/projects" },
@@ -27,8 +26,8 @@ const Services = () => {
 
     return (
         <div className='flex flex-col items-center min-h-screen mx-auto bg-secondary'>
-            <section className='w-full mt-20'>
-                <div className='px-2 flex flex-col items-center justify-center overflow-hidden text-center font-lemonmilk tracking-tighter text-[calc(10vw)] md:text-[calc(8vw)] lg:text-[calc(6vw)] xl:text-[calc(8vw)] z-10 text-light'>
+            <section className='w-full mt-12'>
+                <div className='px-2 flex flex-col items-center justify-center overflow-hidden text-center font-lemonmilk tracking-tighter text-[calc(10vw)] md:text-[calc(8vw)] lg:text-[calc(6vw)] xl:text-[calc(8vw)] 2xl:text-[calc(6vw)] z-10 text-light'>
                     <motion.div
                         initial="hidden"
                         animate="visible"
@@ -58,8 +57,15 @@ const Services = () => {
                 </div>
             </section> 
 
-            <section>
-                <div className="flex items-center justify-center bg-gray-100">
+            <section className="w-full flex flex-col">
+                <div className="w-full flex flex-row items-center justify-center space-x-8">
+                    <DividedBlock />
+                    <DividedBlock />
+                    <DividedBlock />
+                </div>
+                <div className="w-full flex flex-row items-center justify-center space-x-8 mt-8">
+                    <DividedBlock />
+                    <DividedBlock />
                     <DividedBlock />
                 </div>
             </section>           
@@ -68,21 +74,35 @@ const Services = () => {
 };
 
 const DividedBlock: React.FC = () => {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
-      <div className="flex flex-col w-64 h-64">
-        <div className="bg-black  text-light h-1/2">
+      <motion.div
+        className="relative flex flex-col w-[calc(20vw)] h-[calc(20vw)]"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+        <div className="relative z-10 flex flex-col h-full">
+          <div className="flex items-center justify-center flex-1 bg-black text-light">
             <h1>See</h1>
-        </div>
-        <div className="flex flex-row w-full">
+          </div>
+          <div className="flex flex-row w-full h-1/2">
             <div className="flex w-1/2">
-                <img className="object-cover w-32 h-32" src="src/assets/posts/1.png"></img>
+              <motion.img
+                src="src/assets/posts/1.png"
+                className="absolute inset-0 object-cover w-full h-full"
+                initial={{ scale: 1, x: 0, y: 0 }}
+                animate={isHovered ? { scale: 0.5, x: '-25%', y: '25%' } : { scale: 1, x: 0, y: 0 }}
+                transition={{ duration: 0.2 }}
+                alt="Background"
+                />
             </div>
-            <div className="flex flex-row items-end justify-end w-1/2 text-black bg-light">
-                <h1>MORE</h1>
-                <FiArrowRight />
+            <div className="flex flex-row items-center justify-center w-1/2 text-black bg-light">
+              <button className="flex w-full h-full flex-row items-center justify-center">MORE <GoArrowUpRight /></button>
             </div>
+          </div>
         </div>
-      </div>    
+      </motion.div>
     );
   };
 
